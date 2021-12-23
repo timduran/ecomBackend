9884c400b94f1c68aac3ae82a492f43966d631a2
timduran <durantimothy21@gmail.com>
2021-12-22 21:53:12 -0800

    added catergory id

--- routes/category-routes.js
+++ routes/category-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const res = require('express/lib/response');
 const { Category, Product } = require('../models')
 
 // The `/api/categories` endpoint
@@ -28,7 +29,25 @@ router.get('/categories', (req, res) => {
 router.get('/categories/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: ['product_name']
+      }
+    ]
+  })
 })
+.then(dbCategoryData => res.json(dbCategoryData))
+.catch(err => {
+  console.log(err);
+  res.status(500).json(err);
+  });
+});
 
 router.post('/categories', (req, res) => {
   // create a new category
@@ -43,3 +62,6 @@ router.delete('/categories/:id', (req, res) => {
 })
 
 module.exports = router
+
+
+
